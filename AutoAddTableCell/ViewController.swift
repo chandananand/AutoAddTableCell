31b7da1dba414fdb019b857363e4d8ff14d76ebd
chandan <chandan@leuwint.com>
2018-03-27 10:38:02 +0530

    Initial Commit

---
+++ AutoAddTableCell/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AutoAddTableCell
+//
+//  Created by Chandan Anand on 3/27/18.
+//  Copyright © 2018 Chandan Anand. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
